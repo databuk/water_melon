42d9c59c10fc79a31b049af7c7234035aa301975
Dataguy <irinyenikanibukun@gmail.com>
2023-12-16 17:17:33 +0100

    first commit

--- model_architecture.py
+++ model_architecture.py
@@ -7,10 +7,16 @@ from tensorflow.keras.preprocessing.image  import ImageDataGenerator
 
 def build_model():
     model = Sequential()
-    model.add(Conv2D(filters=32, kernel_size=(3,3), padding='same', activation='relu', input_shape=(100, 100, 3)))
-    model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu'))
+    model.add(Conv2D(filters=32, kernel_size=(3,3), padding='same', activation='relu', input_shape=(75, 75, 3)))
+    #model.add(Dropout(0.1))
+    #model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu'))
+    #model.add(Dropout(0.1))
+    #model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu'))
     model.add(Flatten())
+    
     model.add(Dense(32, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
+model = build_model()
+model.summary()
